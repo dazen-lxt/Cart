0b7cdcf3a2cc9218cf96df2e249ac0b7a8c8aa84
cmuoz.altimetrik@ancestry.com <cmuoz.altimetrik@ancestry.com>
2023-05-07 11:27:53 -0500

    Initial Commit

---
+++ Cart/CartApp.swift
@@ -0,0 +1,17 @@
+//
+//  CartApp.swift
+//  Cart
+//
+//  Created by Carlos Mario Muñoz on 7/05/23.
+//
+
+import SwiftUI
+
+@main
+struct CartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
